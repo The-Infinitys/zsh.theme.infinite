156e17ac3828f214afd1b612dae7be4da1caf1cd
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-12-30 18:54:28 +0900

    feat: enable zsh-module

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
+#![feature(trait_alias)]
 mod modules;
 pub use modules::*;
 use once_cell::sync::Lazy;
-
+mod zmod;
 use crate::modules::zsh::theme::{self, prompt_theme::PromptTheme};
-mod build_in;
+use zmod::setup;
 
 static PROMPT_THEME: Lazy<PromptTheme> = Lazy::new(theme::manager::load_theme);
 
 fn prompt_theme() -> &'static PromptTheme {
     &PROMPT_THEME
 }
+
+zsh_module::export_module!(zsh_infinite, setup);
