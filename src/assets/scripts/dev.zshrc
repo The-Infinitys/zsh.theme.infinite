bf2baf9cb3d1a591d40ab6788b7366d211b240f8
The-Infinitys <the.infinity.s.infinity@gmail.com>
2025-12-31 12:49:56 +0900

    feat: zmodule supports

--- src/assets/scripts/dev.zshrc
+++ src/assets/scripts/dev.zshrc
@@ -33,7 +33,6 @@ else
         # モジュール版は Rust 側で PROMPT 変数を直接管理するため、
         # setopt PROMPT_SUBST のみ有効化する
         setopt PROMPT_SUBST
-        zsh-infinite-module
     else
         echo "Error: Failed to load zsh_infinite module from module_path."
         # echo "Falling back to Binary mode..."
--- src/zmod.rs
+++ src/zmod.rs
@@ -1,1 +1,24 @@
+use zsh_system::{Features, ZshModule};
+#[derive(Default)]
+struct ZshInfinite {}
 
+impl ZshModule for ZshInfinite {
+    fn setup(&mut self) -> i32 {
+        0
+    }
+    fn boot(&mut self) -> i32 {
+        0
+    }
+    fn features(&self) -> Features {
+        Features::new()
+    }
+
+    fn cleanup(&mut self) -> i32 {
+        0
+    }
+    fn finish(&mut self) -> i32 {
+        0
+    }
+}
+
+zsh_system::export_module!(ZshInfinite);
